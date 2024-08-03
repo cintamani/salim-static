3e5904719e9b46a65d54c2f2bf0eac307bbc126d
Cintamani Puddu <c.puddu@medify.co.uk>
2024-08-03 22:12:50 +0200

    merge menu on transition

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,44 @@
 // vanilla JS
 
-// document.get
+
+let hasBackground = false;
+
+function transitionToNoBackground(navbar) {
+  navbar.classList.remove('bg-white');
+
+  navbar.querySelectorAll('.text-dark').forEach(function (element) {
+    element.classList.replace('text-dark', 'text-white');
+  });
+
+  hasBackground = false;
+}
+
+function transitionToWhiteBackground(navbar) {
+  navbar.classList.add('bg-white');
+
+  navbar.querySelectorAll('.text-white').forEach(function (element) {
+    element.classList.replace('text-white', 'text-dark');
+  });
+
+  hasBackground = true;
+}
+
+function toggleNavbar(navbar) {
+  const scrollY = window.scrollY;
+
+  if (scrollY > 10 && !hasBackground) {
+    transitionToWhiteBackground(navbar);
+  }
+
+  if (!scrollY) {
+    transitionToNoBackground(navbar);
+  }
+}
+
+const navbar = document.querySelector('.navbar-togglable');
+
+if (navbar) {
+  window.addEventListener('scroll', function () {
+    toggleNavbar(navbar);
+  });
+}
