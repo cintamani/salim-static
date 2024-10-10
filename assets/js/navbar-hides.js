de9858eb4f0b0b15801093404e80cbae4206bfd9
Cintamani Puddu <c.puddu@medify.co.uk>
2024-10-10 23:44:01 +0200

    ff

--- assets/js/navbar-hides.js
+++ assets/js/navbar-hides.js
@@ -1,43 +1,43 @@
-// let hasBackground = false;
+let hasBackground = false;
 
-// function transitionToNoBackground(navbar) {
-//   navbar.classList.remove('custom-bg-colour');
+function transitionToNoBackground(navbar) {
+  navbar.classList.remove('custom-bg-colour');
 
-//   hasBackground = false;
-// }
+  hasBackground = false;
+}
 
-// function transitionToColouredBackground(navbar) {
-//   navbar.classList.add('custom-bg-colour');
+function transitionToColouredBackground(navbar) {
+  navbar.classList.add('custom-bg-colour');
 
-//   hasBackground = true;
-// }
+  hasBackground = true;
+}
 
-// function toggleNavbar(navbar) {
-//   const scrollY = window.scrollY;
+function toggleNavbar(navbar) {
+  const scrollY = window.scrollY;
 
-//   const clientWidth = document.documentElement.clientWidth;
+  const clientWidth = document.documentElement.clientWidth;
 
-//   if (clientWidth < 768 || (scrollY > 10 && !hasBackground)) {
-//     return transitionToColouredBackground(navbar);
-//   }
+  if (clientWidth < 768 || (scrollY > 10 && !hasBackground)) {
+    return transitionToColouredBackground(navbar);
+  }
 
-//   if (!scrollY) {
-//     transitionToNoBackground(navbar);
-//   }
-// }
+  if (!scrollY) {
+    transitionToNoBackground(navbar);
+  }
+}
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   const navbar = document.querySelector('.navbar');
+document.addEventListener('DOMContentLoaded', function () {
+  const navbar = document.querySelector('.navbar');
 
-//   if (navbar) {
-//     toggleNavbar(navbar);
+  if (navbar) {
+    toggleNavbar(navbar);
 
-//     window.addEventListener('scroll', function () {
-//       toggleNavbar(navbar);
-//     });
+    window.addEventListener('scroll', function () {
+      toggleNavbar(navbar);
+    });
 
-//     window.addEventListener('resize', function () {
-//       toggleNavbar(navbar);
-//     });
-//   }
-// });
+    window.addEventListener('resize', function () {
+      toggleNavbar(navbar);
+    });
+  }
+});
